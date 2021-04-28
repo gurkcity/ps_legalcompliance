d0400e0c7630a5bbb3ef8b852c8f5e79a643a24f
Markus-Gurkcity <63096826+Markus-Gurkcity@users.noreply.github.com>
2021-04-28 11:16:22 +0200

    Add composer files and vendor dir

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,14 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit360873948f5380f4375209e0ba3ed463
 {
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->classMap = ComposerStaticInit360873948f5380f4375209e0ba3ed463::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '425b580441b5a7d9b2bc6039fd1d547bb83580dc',
+    'name' => 'prestashop/ps_legalcompliance',
+  ),
+  'versions' => 
+  array (
+    'prestashop/ps_legalcompliance' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '425b580441b5a7d9b2bc6039fd1d547bb83580dc',
+    ),
+  ),
+);
