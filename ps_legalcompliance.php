1e0863cdfc895aedd6c0f3ce67728bee3d7d8be7
Chris Gurk <t_github@gurkcity.de>
2023-07-19 21:24:09 +0200

    new version 8.2.0

--- ps_legalcompliance.php
+++ ps_legalcompliance.php
@@ -65,7 +65,7 @@ class Ps_LegalCompliance extends Module
     {
         $this->name = 'ps_legalcompliance';
         $this->tab = 'administration';
-        $this->version = '8.1.8';
+        $this->version = '8.2.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
         $this->bootstrap = true;
